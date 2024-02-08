62cffea23a9a241d47b307da46f98c96c81894a4
+ <+>
2024-02-09 01:10:06 +0300

    Completed Assignment

---
+++ README.md
@@ -0,0 +1,59 @@
+# To-do List Application <br>
+</br>
+
+
+## Project Description 
+
+### Overview
+This To-Do List Application is a comprehensive task management tool designed to help users organize their daily activities, prioritize tasks, and enhance productivity. Built using a combination of HTML, CSS and vanilla Javascript, the app provides a seamless and intuitive user experience for managing tasks efficiently.
+
+
+### Key Features 
+1. Task Management 
+- Add Tasks: Users can easily add new tasks to their to-do list by  typing in the input field and pressing the "Add" button or the Enter key after entering a new task on the input field.
+- Update Tasks: Tasks can be updated by clicking on the task name. Users can edit the task directly and press Enter to save the changes.
+- Complete Tasks: Users can mark tasks as completed by clicking the checkbox next to the task name.
+- Delete Tasks: Users can delete tasks by clicking the delete icon next to the task.
+ 
+2. Data Persistence 
+- Local Storage: Tasks are stored locally using the browser's localStorage feature ensuring that tasks remain saved even if the browsers is closed, refreshed and reopened again.
+ 
+## Usage
+1. Clone the repository to your local machine:
+~~~
+git clone https://github.com/
+
+~~~
+2. Open the 'index.html' file in your preffered web browser.
+
+
+
+## Contributions 
+
+Contributions to enhance this To-Do List App are welcome! If you'd like to contribute, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch(`git checkout -b feature/new-feature`).
+3. Make your changes.
+4. Commit your changes(`git commit -am "Add new feature"`).
+5. Push to the branch(`git push origin feature/new feature`).
+6. Create a new Pull Request.
+
+## Future Enhancements 
+This To-do List Application is continuously evolving, and some of the future enhancements i may incorporate in the app include the following:
+- Reminders and Notifications: Implementing reminder and notification functionalities to alert the users about upcoming tasks and deadlines.
+- Integration: Integrating with third-party services such as Google Calendar and Trello(Atlassian) for enhanced task management capabilities.
+- User Accounts: Introducing user accounts to enable users to access their to-do lists across multiple devices.
+
+## Conclusion
+Our To-do List Application is designed to simplify task management and improve productivity for users in all aspects of life. With its intuitive interface, robust feature, and of course future enhancemenets, the app aims to become the go-to tool for individuals and teams seeking efficient task organization and management.
+
+## Get Started Today!
+Start organizing your tasks and boosting your productivity with my To-do List Application. Try it out now and experience the difference!
+
+## Licence
+This project is licensed under the `MIT License`.
+
+
+
+
--- script.js
+++ script.js
@@ -1,30 +1,133 @@
-//Loads tasks from localStorage if available
-window.onload = function () {
-    var savedTasks = JSON.parse(localStorage.getItem('tasks'));
-    if(savedTasks) {
-        savedTasks.forEach(function(task){
-            addTaskToDOM(task);
-        });
-    }
+const inputField = document.querySelector('input[type="text"]');
+const todoList = document.querySelector('.todos');
+const addBtn = document.querySelector('#add.btn');
+const itemsLeftElement = document.querySelector('#items-left');
+const allBtn = document.querySelector('#all');
+const activeBtn = document.querySelector('#active');
+const completedBtn = document.querySelector('#completed');
+const clearBtn = document.querySelector('#clear');
 
+
+
+let todos = [];
+//Check if todos exist in local storage
+if(localStorage.getItem("todos")) {
+    todos = JSON.parse(localStorage.getItem("todos"));
+    renderTodoList();
 }
- function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('task-list');
-    var newTask = taskInput.input;
-    addTaskToDOM(newTask);
-    saveTask(newTask);
-    taskInput.value="";
- }
- function addTaskToDOM(task) {
-    var taskList = document.getElementById("taskList");
-    var newTask = document.createElement("li");
-    newTask.textContent = task;
-    taskList.appendChild(newTask);
- }
-  
- function saveTask(task) {
-   var savedTasks = JSON.parse(localStorage.getItem('task')) || [];
-   savedTasks.push(task);
-   localStorage.setItem('tasks', JSON.stringify(savedTasks));
- }
+
+const addTodoItem = () => {
+    if(inputField.value.trim() !== "") {
+        const todoText = inputField.value;
+        inputField.value = "";
+
+        const todoItemId = Math.floor(Math.random() * 10000);
+
+        const newTodoItem = {
+            id: todoItemId,
+            text: todoText,
+            isComplete: false,
+        };
+        todos.push(newTodoItem);
+        localStorage.setItem("todos", JSON.stringify(todos));
+
+        renderTodoList();
+    }
+};
+ inputField.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && inputField.value.trim() !== "") {
+        addTodoItem();
+    }
+ });
+   addBtn.addEventListener("click", () => {
+    addTodoItem();
+   })
+
+   //function for rendering the TodoList
+   function renderTodoList() {
+    todoList.innerHTML = "";
+    todos.forEach((todo, index) => {
+        const newTodoItem = document.createElement('li');
+        newTodoItem.className = "card todo-item";
+        newTodoItem.setAttribute("draggable", "true");
+        newTodoItem.setAttribute('data-index', index)
+        const todoContent = `
+        <div class = "todo">
+        <input type = "checkbox" id="check-box-${todo.id}" 
+        ${todo.isComplete ? "checked" : "" }> 
+        <label for="check-box-${todo.id}></label>
+        <p>${todo.text}</p>
+        </div>
+     
+        `;
+        newTodoItem.innerHTML = todoContent;
+        todoList.appendChild(newTodoItem);
+    });
+    addDraggableEventListeners();
+}
+// Drag and Drop functionality 
+let dragStartIndex;
+function addDraggableEventListeners() {
+    const todoItems = document.querySelectorAll(".todo-item");
+
+    todoItems.forEach((item, index)=> {
+        item.addEventListener("dragstart", () => 
+        item.classList.add("dragging"))
+
+        item.addEventListener("dragend", () => {
+            item.classList.remove("dragging")
+            updateTodosOrder();
+        })
+    })
+    todoList.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const draggingItem = document.querySelector(".dragging");
+        //get array of all items except the one dragging
+        let siblings = [...todoList.querySelectorAll(".todo-item:not(.dragging")];
+         
+        //find next sibling item after which the dragging item should be placed
+         let nextSibling = siblings.find(sibling => {
+            return e.clientY <= sibling.offsetTop + sibling.offSetHeight / 2;
+         });
+         // Inserting the dragging item before the found sibling
+         todoList.insertBefore(draggingItem, nextSibling);
+    });
+    todoList.addEventListener('dragenter', e => e.preventDefault());
+}
+function updateTodosOrder() {
+    const updatedTodos = [];
+    const todoItems = document.querySelectorAll('.todo-item');
+
+    todoItems.forEach((item) => {
+        const index = parseInt(item.getAttribute("data-index"));
+        updatedTodos.push(todo[index]);
+    });
+    todos = updatedTodos;
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//function for updatind todo list item completion status
+
+
+//function for Deleting Todo item 
+const deleteItem = () => {
+
+};
+// function for updating Items left count
+const updateItemsLeft = () => {
+    const incompleteItems = todos.filter((todo) => !todo.isComplete);
+    itemsLeftElement.textContent = incompleteItems.length;
+}
+ 
+// function for clearing completed todos
+function clearCompletedTodos() {
+    todos = todos.filter((todo) => !todo.isComplete);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderTodoList();
+    updateItemsLeft();
+}
+clearBtn.addEventListener('click', () => {
+    clearCompletedTodos();
+}
+)
